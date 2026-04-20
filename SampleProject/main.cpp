edb19ff92bac85586594911985ca35e048107b8f
= <scvfufa@gmail.com>
2026-04-20 14:49:02 +0900

    feat::캐릭터 관련 변수 입,출력 실습

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -4,15 +4,45 @@ using namespace std;
 
 int main() {
 
-	char userName[50] = ""; //선언, 초기화
+	char userName[50];
+	char charactorClass[50];
 
-	cout << "[ User Name ] \n";
+	int level = 1;
+	int hp = 100;
+	int mp = 100;
+	float attackSpeed = 5.5f;
+
+	int strength = 50;
+	int dexterity = 50;
+	int vitality = 50;
+	int energy = 50;
+
+	bool isHardcore = true;
+
+	cout << "[ Character Creation ]\n";
 	cout << "Input your name : ";
+	cin >> userName;
+	cout << "Input your Class : ";
+	cin >> charactorClass;
+	cout << "\n:::::::: Welcome to the Sanctuary ::::::::\n";
+	cout << "User Name : [" << userName << "]\n";
+	
+	cout << "----------------Character Status----------------\n";
+	cout << "Class : " << charactorClass << "\n";
+	cout << "Level : " << level << "\n";
+	cout << "Hp : " << hp << "\n";
+	cout << "MP : " << mp << "\n";
+	cout << "Attack Speed : " << attackSpeed << "\n";
+	cout << "Strength : " << strength << "\n";
+	cout << "Dexterity : " << dexterity << "\n";
+	cout << "Vitality : " << vitality << "\n";
+	cout << "Energy : " << energy << "\n";
+	cout << "Hardcore Mode : " << isHardcore << " (1: true, 0: flase)\n";
+	cout << "------------------------------------------------\n";
 
-	cin >> userName;  // 변수에 입력받은 값을 할당.
+	cout << "[Memory Check] int type size : " << sizeof(hp) << "bytes\n";
+	cout << "[Memory Check] bool type size : " << sizeof(isHardcore) << "bytes\n";
 
-	cout << "::::::Welcome to the D&D World:::::: \n";
-	cout << "User Name : [" << userName << "]!\n";
 
 
 	return 0;
